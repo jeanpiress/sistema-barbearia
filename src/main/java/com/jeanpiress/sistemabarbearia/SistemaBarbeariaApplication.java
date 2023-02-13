ab1c133982bf0f9730f7fd86d552622f4d3cd3a5
jeanpiress <jean.m.pires@gmail.com>
2023-02-13 14:24:06 -0300

    Projeto criado

---
+++ src/main/java/com/jeanpiress/sistemabarbearia/SistemaBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.jeanpiress.sistemabarbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBarbeariaApplication.class, args);
+	}
+
+}
