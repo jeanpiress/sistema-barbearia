df19f1b75bede934c6e17912ce16c67bea81cf6f
Jean Pires <jean.m.pires@gmail.com>
2023-07-24 15:34:11 -0300

    profissionais criado

---
+++ br-barbeiros/src/main/java/com/jeanpiress/barbeiros/BrBarbeirosApplication.java
@@ -0,0 +1,13 @@
+package com.jeanpiress.barbeiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrBarbeirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrBarbeirosApplication.class, args);
+	}
+
+}
