d537909c02fe61b0de38b48110fd0df4220de571
Jean Pires <jean.m.pires@gmail.com>
2023-08-14 21:39:52 -0300

    Feing client de Profissional implementado

---
+++ br-financeiro/src/main/java/com/jeanpiress/brFinanceiro/feignclients/ProfissionalFeignClient.java
@@ -0,0 +1,21 @@
+package com.jeanpiress.brFinanceiro.feignclients;
+
+import java.util.Optional;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.jeanpiress.brFinanceiro.entity.Profissional;
+
+@Component
+@FeignClient(name = "br-producao", url = "localhost:8001", path = "/profissionais")
+public interface ProfissionalFeignClient {
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Optional<Profissional>> buscarPorId(@PathVariable Long id);
+		
+	
+}
--- br-financeiro/src/main/java/com/jeanpiress/brFinanceiro/service/SalarioService.java
+++ br-financeiro/src/main/java/com/jeanpiress/brFinanceiro/service/SalarioService.java
@@ -1,13 +1,22 @@
 package com.jeanpiress.brFinanceiro.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jeanpiress.brFinanceiro.entity.Profissional;
 import com.jeanpiress.brFinanceiro.entity.Salario;
+import com.jeanpiress.brFinanceiro.feignclients.ProfissionalFeignClient;
 
 @Service
 public class SalarioService {
+	
+	@Autowired
+	private ProfissionalFeignClient profissionalFeignClient;
 
 	public Salario getSalario(Long profissionalId) {
-		return new Salario("Jean", 1000.0, 4000.0);
+		Profissional profissional = profissionalFeignClient.buscarPorId(profissionalId).getBody().get();
+		
+		
+		return new Salario(profissional.getNome(), 1000.0, 4000.0);
 	}
 }
