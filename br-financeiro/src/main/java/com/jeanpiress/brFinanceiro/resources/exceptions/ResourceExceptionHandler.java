1a4353d0af064ca1eb6c4ba8e8711270da0011e6
Jean Pires <jean.m.pires@gmail.com>
2023-09-14 11:54:55 -0300

    tratamento excessoes em br-financeiro

---
+++ br-financeiro/src/main/java/com/jeanpiress/brFinanceiro/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.jeanpiress.brFinanceiro.resources.exceptions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String mensagemUsuario = "Mensagem invalida";
+		String mensagemDesenvolvedor = ex.getCause().toString();
+		List<DescricaoErros> erros = Arrays.asList(new DescricaoErros(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	public ResponseEntity<Object> handleErroDeleteRecursoNaoEncontrado(EmptyResultDataAccessException ex,
+			WebRequest request) {
+		String mensagemUsuario = "recurso não existe";
+		String mensagemDesenvolvedor = ex.toString();
+		List<DescricaoErros> erros = Arrays.asList(new DescricaoErros(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	@ExceptionHandler({NoSuchElementException.class})
+	public ResponseEntity<Object> handleRecursoNaoEncontrado(NoSuchElementException ex,
+			WebRequest request) {
+		String mensagemUsuario = "recurso não existe";
+		String mensagemDesenvolvedor = ex.toString();
+		List<DescricaoErros> erros = Arrays.asList(new DescricaoErros(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	
+}
