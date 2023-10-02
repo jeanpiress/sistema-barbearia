a99602b5dec229af4487eb18de581f9403c27ccd
Jean Pires <jean.m.pires@gmail.com>
2023-10-02 14:15:16 -0300

    Config server implementado

---
+++ br-config-server/src/main/java/com/jeanpiress/brconfigserver/BrConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.jeanpiress.brconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class BrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrConfigServerApplication.class, args);
+	}
+
+}
---
+++ br-config-server/src/test/java/com/jeanpiress/brconfigserver/BrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanpiress.brconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
