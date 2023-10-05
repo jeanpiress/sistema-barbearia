bd2619248af9fc4f3a17e9a86a121d389fcb4991
Jean Pires <jean.m.pires@gmail.com>
2023-10-05 10:40:10 -0300

    login com jwt implementado

--- br-oauth/src/main/java/com/jeanpiress/broauth/config/AppConfig.java
+++ br-oauth/src/main/java/com/jeanpiress/broauth/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
-public class AppConfig {
+public class AppConfig  {
 
 	@Bean
 	public BCryptPasswordEncoder BCryptPasswordEncoder() {
